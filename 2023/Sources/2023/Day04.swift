6ecbf17c2e1afe8b1a03225145877c8084644273
Teameh <1330668+teameh@users.noreply.github.com>
2023-12-04 21:59:50 +0100

    Day 4

--- 2023/Sources/2023/Day04.swift
+++ 2023/Sources/2023/Day04.swift
@@ -2,11 +2,51 @@ import Foundation
 import Algorithms
 
 struct Day4: Day {
+    let lines = Files.input04Txt
+        .read()
+        .split(separator: "\n")
+
     func partOne() -> String {
-        ""
+        lines
+            .map { card in
+                let (winningNumbers, numbers) = parse(card: card)
+                return numbers
+                    .filter { winningNumbers.contains($0) }
+                    .reduce(1) { acc, _ in acc * 2 } / 2
+            }
+            .reduce(0, +)
+            .description
     }
 
     func partTwo() -> String {
-        ""
+        lines
+            .reversed()
+            .reduce(into: [Int]()) { (copiesWonForEachCard: inout [Int], card) in
+                let cardIndex = copiesWonForEachCard.count
+                let (winningNumbers, numbers) = parse(card: card)
+                let result = numbers
+                    .filter { winningNumbers.contains($0) }
+                    .enumerated()
+                    .reduce(into: 0) { (acc: inout Int, iteration: (Int, Int)) in
+                        acc += copiesWonForEachCard[cardIndex - (iteration.0 + 1)]
+                    }
+
+                copiesWonForEachCard.append(1 + result)
+            }
+            .reduce(0, +)
+            .description
+    }
+
+    func parse(card: Substring) -> (winningNumbers: [Int], numbers: [Int]) {
+        let parts = card.split(separator: #/[:|]/#)
+        let winningNumbers = parts[1]
+            .matches(of: #/\d+/#)
+            .map { Int(card[$0.range]) }
+
+        let numbers = parts[2]
+            .matches(of: #/\d+/#)
+            .map { Int(card[$0.range]) }
+
+        return (winningNumbers, numbers)
     }
 }
--- 2023/Sources/2023/main.swift
+++ 2023/Sources/2023/main.swift
@@ -7,7 +7,7 @@ func runAllDays() {
         (Day1.self, "55607", "55291"),
         (Day2.self, "2283", "78669"),
         (Day3.self, "535351", "87287096"),
-        (Day4.self, "", ""),
+        (Day4.self, "18653", "5921508"),
         (Day5.self, "", ""),
         (Day6.self, "", ""),
         (Day7.self, "", ""),
@@ -18,6 +18,7 @@ func runAllDays() {
 
     days.forEach { day, resultOne, resultTwo in
         let result = day.run()
+
         assert(result.0 == resultOne, "\(result.0) != \(resultOne)")
         assert(result.1 == resultTwo, "\(result.1) != \(resultTwo)")
     }
